c96d7cb3850ae194517fc07bccacf4719dfaa37b
FilHarr <49679000+FilHarr@users.noreply.github.com>
2020-08-17 00:16:21 +0100

    Fix healthcheck to use HTTPS port

--- healthcheck-4.1.x.sh
+++ healthcheck-4.1.x.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-MANAGE_HTTP_PORT=$MANAGE_HTTP_PORT
+MANAGE_HTTPS_PORT=$MANAGE_HTTPS_PORT
 
-# check if MANAGE_HTTP_PORT variable has been set. If not use default HTTP port for healthcheck, otherwise use set port.
+# check if MANAGE_HTTPS_PORT variable has been set. If not use default HTTPS port for healthcheck, otherwise use set port.
 
-if [ -z "$MANAGE_HTTP_PORT" ]
+if [ -z "$MANAGE_HTTPS_PORT" ]
 then
-  wget --quiet --tries=1 --no-check-certificate http://127.0.0.1:8088 || exit 1
+  wget --quiet --tries=1 --no-check-certificate http://127.0.0.1:8043 || exit 1
 else
-  wget --quiet --tries=1 --no-check-certificate http://127.0.0.1:$MANAGE_HTTP_PORT || exit 1
+  wget --quiet --tries=1 --no-check-certificate -O /dev/null --server-response --timeout=5 https://127.0.0.1:$MANAGE_HTTPS_PORT || exit 1
 fi
